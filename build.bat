fd93c58bdb72447cc0b5c3d87d30c0e362589a52
lwh9346 <lwh9346@163.com>
2020-03-22 10:06:32 +0800

    修改输出文件名

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 mkdir build
 set CGO_ENABLED=0
 set GOOS=windows
-go build -o build/Updater_windows.exe
+go build -o build/MAU_windows.exe
 set GOOS=linux
-go build -o build/Updater_linux
+go build -o build/MAU_linux
 set GOOS=darwin
-go build -o build/Updater_darwin
+go build -o build/MAU_darwin
